0a3afa8f979ca24a649a149865c5620d3f198223
mohitsoni9399 <mohitsoni02184@gmail.com>
2024-08-26 12:13:53 +0530

    Add files via upload

---
+++ module_six.java
@@ -0,0 +1,215 @@
+import java.util.Scanner;
+
+
+
+public class module_six{
+
+public static void main(String[] args) {
+    
+/* 
+    // type promotion
+       
+    //   byte b=5;
+    //   byte a =(byte)(b*2);
+    //   System.out.println(a);
+   
+    //    int a=10;
+    //    float b=20.25f;
+    //    long c=25;
+    //    double d=30;
+    //    double ans=a+b+c+d;
+    //    System.out.println(ans);
+    
+    // short a=5;
+    // byte b=25;
+    // char c='c';
+    // byte bt = (byte)(a+b+c);
+    // System.out.println(bt);
+
+    // char a= 'a';
+    // char b='b';
+    
+    // char c=a-b;
+    // System.out.println(c);
+    
+    // System.out.println((int)a);
+    // System.out.println((int)b);
+    // System.out.println(b-a);
+    // System.out.println(a);
+ 
+ */
+ 
+ 
+ 
+ 
+    /* 
+    // type casting:-
+     
+     float a=25.12f;
+     int b=(int) a;
+     System.out.println(b);
+
+     double db=3343.3434;
+     float fe=(float) db; 
+
+     char ch='a';
+     int  number = ch; // 'c' 'd'-------
+     System.out.println(number);
+
+  */
+  
+  
+  
+  
+  
+  
+    /* 
+  
+    //type conversion with compatible
+
+//   int a=25;
+//   long b=a;
+//   System.out.println(b);
+
+// long a=25;
+// int b=a;
+// System.out.println(b);
+
+
+*/
+
+
+ /* 
+  //sum of a and b with inputs:-
+     
+     
+     // also taking float:-
+     
+     Scanner sc= new Scanner(System.in);
+     System.out.println("eneter the first numeber");
+     int a=sc.nextInt();
+     System.out.println("enter the second numer");
+     int b=sc.nextInt();
+      int sum=a+b;
+       
+      int product=a*b;
+     System.out.println("the sum is="+sum);
+     System.out.println("the product is="+product);
+      System.out.println("enetr the raduius");
+    
+      float r = sc.nextFloat();
+      float area;
+    area=3.14f*r*r ;
+    System.out.println("The area is ="+ area);
+
+*/
+    
+
+
+
+  
+  
+  
+  
+  
+    /*  
+   // Inputs in java :-
+
+   Scanner sc = new Scanner(System.in);
+   System.out.println("enter the input");
+    String input= sc.next();  single word
+    System.out.println("the input is "+ input);
+   
+   System.out.println("enter the name");
+    String name= sc.nextLine(); //paragraph
+    System.out.println(name);
+
+   int number = sc.nextInt(); //Integer
+   System.out.println(number);
+  float price=sc.nextFloat();
+    System.out.println(price);
+
+       boolean bol=sc.nextBoolean();
+       System.out.println(bol);
+
+   */
+
+
+
+  
+   
+   
+   
+   
+   
+   
+    /*   4. sum of a and b :-
+    
+    int a=10,b=20;
+    int sum;
+    sum=a+b;
+    System.out.println("sum is "+sum);
+    
+    */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*  3. Datatypes in java:-
+      byte b=8;
+      System.out.println(b);
+      char ch = 'a';
+      System.out.println(ch);
+      boolean var = true;
+      System.out.println(var);
+      float price = 10.5f;
+      int number = 25;
+      // long 
+      // double
+      short n=240;
+      System.out.println(n);
+
+      */
+
+
+
+
+
+    /* 2. variables in java:-
+     
+     int a=10;
+     int b=5;
+     String name ="Tony Stark";
+     
+     System.out.println(a);
+     System.out.println(name);
+     
+     a=50;
+     System.out.println(a);
+    
+      a=b;
+     System.out.println(a);
+
+*/
+
+/* 1. output in java:-
+   
+   System.out.print("hello world\n"); //line ka space
+   System.out.println("hello world");
+   System.out.println("hello world"); 
+   */
+
+
+
+
+}
+
+
+} 
+//boiler plate code

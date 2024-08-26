454470ed48825180b0517fd1ed9d24c44f816ac5
mohitsoni9399 <mohitsoni02184@gmail.com>
2024-08-26 12:16:33 +0530

    Add files via upload

---
+++ assignment_six.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+
+public class assignment_six {
+    
+public static void main(String[] args) {
+                         // module 6 assignment
+    
+    
+    //         1.  Average of the numbers:-
+
+    // Scanner sc = new Scanner (System.in);
+    // System.out.println("Enter the three numbers:-");
+    // float a,b,c;
+    // a= sc.nextFloat();
+    // b=sc.nextFloat();
+    // c=sc.nextFloat();
+    // float average = (a+b+c)/3;
+    // System.out.println("The average of three numbers is = "+ average);
+
+   // 2.      Area of Square   
+
+//    Scanner sc = new Scanner (System.in);
+//    System.out.println("Enter the tside of square:-");
+//    int s=sc.nextInt();
+//    int area = s*s;
+//    System.out.println("The area of the square is = "+area);
+
+
+   // 3.      calculate the price with gst 18%
+
+   /* 
+   Scanner sc = new Scanner (System.in);
+   float a,b,c,total;
+   
+   System.out.println("Enter the cost of pen:-");
+   a=sc.nextFloat();
+   
+   System.out.println("Enter the cost of pencil :-");
+   b=sc.nextFloat();
+   
+   System.out.println("Enter the cost of  eraser:-");
+   c=sc.nextFloat();
+   
+   total=a+b+c;
+   System.out.println("The total cost of items = "+total);
+
+   // cost with gst 
+    float totalcost;
+
+    totalcost= (0.18f*total) +total;
+    System.out.println();
+    System.out.println("The total cost with gst is ="+ totalcost);
+
+   */
+   
+  
+
+
+
+
+
+
+
+}
+
+}
